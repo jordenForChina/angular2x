544c38cfd6a4b0120fc48306c2dd33693bccec58
lixiang <freewm@163.com>
2017-06-19 11:25:55 +0800

    finish

--- src/app/common-module/common-module.module.ts
+++ src/app/common-module/common-module.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BaseDirective } from './base.directive';
+import { HostListenerDirective } from './host-listener.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [BaseDirective],
-  exports:[BaseDirective]
+  declarations: [BaseDirective, HostListenerDirective],
+  exports:[BaseDirective,HostListenerDirective]
 })
 export class CommonModuleModule { }
---
+++ src/app/common-module/host-listener.directive.ts
@@ -0,0 +1,23 @@
+import { Directive ,ElementRef, HostListener, Input} from '@angular/core';
+
+@Directive({
+  selector: '[appHostListener]'
+})
+export class HostListenerDirective {
+
+  constructor(private el:ElementRef) { };
+
+  // 更换颜色
+  hightLight(color){
+    this.el.nativeElement.style.color = color;
+  }
+  // 监听
+  @HostListener('mouseenter') onMouseEnter(){
+    this.hightLight('purple');
+  };
+
+  @HostListener('mouseleave') onMouseLeave(){
+    this.hightLight('red');
+  }
+
+}
